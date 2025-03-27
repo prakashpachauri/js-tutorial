02eaed1b86e3198a84d45657b3562d30169d0901
prakashpachauri <prakashpachauri3128@gmail.com>
2025-03-27 14:35:21 +0000

    add some change

--- Basics/data.js
+++ Basics/data.js
@@ -1,5 +1,26 @@
 "use strict"; // treat all js code in newer version
 
-// aert(3+3)
+// alert(3+3)// we are use nodejs,not browser//
 
-console.log(3+3)
+
+// Code Readability that should be High
+
+console.log(3+3);
+console.log("Hello")
+
+let name="Prakash"
+let age =19
+let isloggedin=false
+let state 
+
+//number =>2 power 53
+// bigint
+// string =""
+// boolean= true/false
+// null = standalone value
+// undefined =>
+// symbol => unique
+//object
+
+console.log(typeof age);
+
