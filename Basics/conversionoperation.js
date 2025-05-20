3253ba6cd473cfb6ca4003ea04f62a2f0c4b5d54
prakashpachauri <prakashpachauri3128@gmail.com>
2025-05-20 10:28:31 +0000

    some new file and changes

---
+++ Basics/conversionoperation.js
@@ -0,0 +1,27 @@
+// console.log("Javascript Tutorial")
+let score="Prakash"
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// // "33"=> 33
+// // "33abc"=> NaN
+// // true => 1, false => 0
+
+let valueInNumber=Number (score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+let isloggedin= 1
+let booleanisloggedin=Boolean(isloggedin)
+console.log(booleanisloggedin);
+
+// >> 1 =true , 0 => false
+// "" => false
+// "
+// "hitesh"=>true
+
+let somenumber=33
+    let stringnumber=String(somenumber)
+    console.log(stringnumber);
+    console.log(typeof stringnumber);
+    
---
+++ Basics/variable.js
@@ -0,0 +1,17 @@
+const accountId=255
+let accountEmail="Prakashpachauri@gmail.com"
+var accountPassword="12345"
+
+/* Prefer not to use var because of issue  in block of Scope and functional Scope*/
+accountCity="Firozabad"
+
+// accountId=123 // not allowed
+accountEmail="Prakash@gmail.com"
+accountPassword="4569874"
+accountCity="mathura"
+let accountState;
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
+
+console.log(accountId);
+
+
