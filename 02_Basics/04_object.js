06039c5bf056db97a37e2c3a4108b012e5ddcc98
prakashpachauri <prakashpachauri3128@gmail.com>
2025-06-06 13:56:29 +0000

    some additon comment in this file 2025

--- 02_Basics/04_object.js
+++ 02_Basics/04_object.js
@@ -1,5 +1,9 @@
-// const tinderUser= new Object()  
+// const tinderUser= new Object()  // this object is created with the constructor
 // it is a Singleton object in javascript
+
+// non singleton object 
+
+// declare the object and intialize the key and value in the object
 const tinderUser = {}
 tinderUser.id ="123ab"
 tinderUser.name ="Prakash Pachauri"
@@ -7,6 +11,7 @@ tinderUser.name ="Prakash Pachauri"
 tinderUser.isloggedIn =false
 // console.log(tinderUser);
 
+// nested object :- it is a form of  object and in one object contain many object in the js
 const regularUser={
    userdetail: {
         userfulname: {
@@ -17,17 +22,26 @@ const regularUser={
     }
 }
 
+// how to print the nested object in the javascript
+
 console.log(regularUser.userdetail.userfulname.firstname);
+
+// declare the object and intializes the object key and value
 
 const obj1={ 1:"a", 2:"b"}
 const obj2={ 3:"a", 4:"b"}
 const obj4={ 5:"a", 6:"b"}
 
+// how to print the combine of the key and value
+
 // const obj4={obj1,obj2}
+// object.assign :- it is used for printing the combine and add the many object key value in the one object it solve the problem of printing
 const obj3=Object.assign({},obj1,obj2,obj4)
 console.log(obj3);
 console.log(obj1);
 
+
+// how to declare the array and intialize the key and value in the array array is represent the [] 
 
 const user=[
     {
@@ -48,11 +62,27 @@ const user=[
     },
 ]
 
+// how to print the array with the help of indexing method and .email
+
 console.log(user[1].email)
 
 // console.log(tinderUser);
+
+// how to print the key of the object
+
+
 // console.log(Object.keys(tinderUser));
+
+// how to print the value of object.values(tinderUser)
+
+
 // console.log(Object.values(tinderUser));
+
+// how to retreive the data from the object
+
+
 // console.log(Object.entries(tinderUser));
+
+
 // console.log(tinderUser.hasOwnProperty('isloggedIn'));
 
