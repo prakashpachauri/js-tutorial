8dffeff994110d6fe795edba22d4493b494a3ad4
prakashpachauri <prakashpachauri3128@gmail.com>
2025-08-21 13:27:45 +0000

    random bgcolor changer

---
+++ 04_controlflow/one.js
@@ -0,0 +1,21 @@
+// if statement:-  it is a conditonal based statement and it is used to check the condition of the program it is true or else is a default condition
+
+const temperaturis=5
+const isuserloggedIn=true
+
+if(temperaturis<50){
+    console.log("less than 50");
+    
+}else{
+    console.log("greater than 50");
+   
+    
+} 
+
+
+const score=200
+if(score>=100){
+    let power="fly"
+    console.log(`User power, ${power}`)}
+    
+// } console.log(`Score is greater ${power}`)
