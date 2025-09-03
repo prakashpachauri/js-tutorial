ef20807dabb867bc6ffa2f2fb94977dc4a05649d
prakashpachauri <prakashpachauri3128@gmail.com>
2025-09-03 12:43:56 +0000

    fetch asyc await discussion

--- 09_advanceone/promises.js
+++ 09_advanceone/promises.js
@@ -28,4 +28,88 @@ new Promise (function(resolve,reject) {
 
 // Promise Three
 
-const 
+const promiesThree = new Promise (function(resolve, reject){
+    setTimeout(function(){
+        resolve({ username: 'Prakash', email: 'Prakashpachauri@example.com'})
+    },1000)
+})
+
+promiesThree.then( function(user){
+    console.log(user)
+})
+
+
+
+
+// Promise Four
+
+const promiseFour = new Promise (function(resolve, reject){
+    setTimeout(function(){
+        let error = false;
+        if(!error){
+            resolve({ username: 'Hiteshchaudhy', pass: '123'})
+        } else{
+            console.log("ERROR : Something went wrong")
+        }
+    })
+})
+
+promiseFour.then((user) => {
+    console.log(user)
+    return user.username;
+
+}).then( (username) =>{
+    console.log(username)
+}).catch( function(error){
+    console.log(error)
+}).finally( () => console.log("Promise is either resole or rejected finally executed")
+)
+
+
+
+// Promise five - Promise chaining
+
+const promiseFive= new Promise ( function(resolve, reject){{
+    setTimeout(function(){
+        let error=false;
+        if(!error){
+            resolve({username:"javascript" ,password:"123"})
+        }else{
+            console.log("Error: js went wrong")
+        }
+    })
+}})
+
+async function consumepromisefive() {
+    try {
+        const response = await promiseFive
+        console.log(response)
+    } catch (error) {
+        console.log(error)
+        
+    }
+    
+}
+consumepromisefive()
+
+
+// async function getallusers(){
+    
+// try {
+//     const response = await fetch('https://jsonplaceholder.typicode.com/users')
+//     const data = await response.json()
+//     console.log(data)  
+    
+// } catch (error) {
+//     console.log("E:",error)
+    
+// }
+// }
+
+// getallusers()
+
+fetch('https://jsonplaceholder.typicode.com/users')
+.then( (response) => response.json())
+.then( (data) => console.log(data))
+.catch( (error) => console.log("E:",error))
+.finally( () => console.log("Finally executed"))
