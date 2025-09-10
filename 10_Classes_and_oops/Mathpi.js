31fbbed01775b937631c095aaf1c645cc1ab2f74
prakashpachauri <prakashpachauri3128@gmail.com>
2025-09-10 13:51:56 +0000

    advance file math.pi

---
+++ 10_Classes_and_oops/Mathpi.js
@@ -0,0 +1,41 @@
+const Descripter=Object.getOwnPropertyDescriptor(Math,"PI")
+console.log(Descripter);
+
+
+// console.log(Math.PI)
+// Math.PI=5
+// console.log(Math.PI)
+
+const chai={
+    name:"Ginger Chai",
+    Price:"250",
+    isAvailable:true,
+
+    orderbook: function(){
+        console.log("chai nahi bani")
+    }
+}
+
+// console.log(chai)
+console.log(Object.getOwnPropertyDescriptor(chai,'name'));
+// Object.defineProperty(chai,'name',{
+//     writable:false,
+//     enumerable:false,
+//     // configurable:false,
+
+// })
+
+// console.log(Object.getOwnPropertyDescriptor(chai,'name'));
+
+
+for (let [key,value] of Object.entries(chai)) {
+    if (typeof value!=='function'){
+        
+        console.log(`${key}: ${value}`);
+    }
+    
+    
+    
+}
+
+
